eb4d70ada0381c9843da52996f46ec83508da5e5
tgibbons-css <tgibbons@css.edu>
2017-02-27 10:14:24 -0600

    Initial commit

---
+++ app/src/main/java/css/cis3334/multipleactivities/SecondActivity.java
@@ -0,0 +1,36 @@
+package css.cis3334.multipleactivities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView textViewData;
+    String name;
+    Double num;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        textViewData = (TextView) findViewById(R.id.textViewData);
+
+        Bundle extras = getIntent().getExtras();
+        name = extras.getString("MainName");
+        num = extras.getDouble("MainNumber");
+        textViewData.setText("Name : " + name + " Number : " + num);
+
+    }
+
+    @Override
+    public void finish() {
+        Intent intent = new Intent();
+        intent.putExtra("NewName", name+"@css.edu");
+        intent.putExtra("NewNum", num * 2);
+        setResult(RESULT_OK, intent);
+        super.finish();
+    }
+}
